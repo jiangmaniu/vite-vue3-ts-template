4d955793308ad0aefaafb378a7fd831ab67d34d5
江麻妞 <jiangmaniu@qq.com>
2021-04-22 17:38:34 +0800

    chore(eslint): 添加 eslint 集成

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+// 以下所有属性的值为覆盖的值，其他未显示的设置属性都为默认值
+// https://prettier.io/docs/en/options.html
+module.exports = {
+  printWidth: 100,
+  semi: false,
+  vueIndentScriptAndStyle: true,
+  singleQuote: true,
+  htmlWhitespaceSensitivity: 'strict',
+}
