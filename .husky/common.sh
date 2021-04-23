73d5cd4865f2e3636c3b87d10f6568496de49985
江麻妞 <jiangmaniu@qq.com>
2021-04-23 15:02:59 +0800

    chore(husky): 新增 husky 兼容 window10 文档地址

--- .husky/common.sh
+++ .husky/common.sh
@@ -4,6 +4,7 @@ command_exists () {
 }
 
 # Workaround for Windows 10, Git Bash and Yarn
+# https://typicode.github.io/husky/#/?id=yarn-on-windows
 if command_exists winpty && test -t 1; then
   exec < /dev/tty
 fi
